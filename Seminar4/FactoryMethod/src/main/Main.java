2063c19f11d89977af90531ba59f696a73cf5f0c
stefanclaudiu19 <stefanclaudiu19@stud.ase.ro>
2022-04-01 18:22:15 +0300

    Singleton, SimpleFactory, FactoryMethod Main class modificari

--- Seminar4/FactoryMethod/src/main/Main.java
+++ Seminar4/FactoryMethod/src/main/Main.java
@@ -1,3 +1,8 @@
+// 1. Companie de transport in comun - STB.
+//  Trebuie implementat un modul care sa creeze obiecte de tipul MijlocTransport: Autobuz, Tramvai, Troleibuz
+//  Modulul trebuie sa ajute la crearea de obiecte de familia de clase MijlocTransport
+//  Tipurile de transport sunt salvate intr-un enum{Autobuz, Tramvai, Troleibuz}.
+
 package main;
 
 import clase.MijlocTransport;
--- Seminar4/SimpleFactory/src/main/Main.java
+++ Seminar4/SimpleFactory/src/main/Main.java
@@ -1,25 +1,9 @@
-package main;
-
 // 1. Companie de transport in comun - STB.
 //  Trebuie implementat un modul care sa creeze obiecte de tipul MijlocTransport: Autobuz, Tramvai, Troleibuz
 //  Modulul trebuie sa ajute la crearea de obiecte de familia de clase MijlocTransport
 //  Tipurile de transport sunt salvate intr-un enum{Autobuz, Tramvai, Troleibuz}.
 
-//  2. Pentru crearea de obiecte de tip Autobuz sunt consumate foarte multe resurse.
-//  Din acest motiv trebuie propusa o varianta prin care daca exista deja un obiect creat,
-//  sa fie folosit acest obiect pentru viitoarele obiecte,
-//  fara a mai fi nevoie de crearea de la 0 a obiectelor de tip Autobuz.
-//  Aceeasi regula se va aplica si pentru alte tipuri de mijloace de transport.
-
-//  3. Pentru obiectele de tipul AutobuzLinie se doreste ca
-//  in momentul in care au fost create obiectele de acest tip sa nu mai poata fi modificate.
-//  De asemenea pentru crearea unui obiect de tipul AutobuzLinie trebuie precizat modelul de autobuz folosit,
-//  soferul care il va conduce, daca va avea opriri la capat de linie,
-//  daca deschide usile in fiecare statie fara ca pasagerii sa solicite acest lucru,
-//  textul afisat de ecranul derulator si alte elemente.
-//  Unele atribute sunt optionale avand o valoare prestabilita.
-//  Se doreste implementarea modulului care se va ocupa de initializarea acestor obiecte,
-//  nefiind necesara introducerea atributelor optionale, iar obiectul odata creat sa nu mai poata fi modificat.
+package main;
 
 import clase.MijlocTransport;
 import clase.TipMijlocTransport;
